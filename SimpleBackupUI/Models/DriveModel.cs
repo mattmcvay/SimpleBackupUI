0176b38fa0157ae5cf61c73d515f7197e2846fd9
matt.mcvay <matt.mcvay@gmx.com>
2023-04-03 07:22:45 -0400

    4.3.2023 commit - Got the basics working

---
+++ SimpleBackupUI/Models/DriveModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBackupUI.Models
+{
+    public class DriveModel
+    {
+        public string Name { get; set; }
+        public string DriveType { get; set; }
+        public string DriveFormat { get; set; }
+        public string AvailableFreeSpace { get; set; }
+        public string TotalSize { get; set; }
+        public string VolumeLabel { get; set; }
+
+    }
+}
---
+++ SimpleBackupUI/Scripts/custom.js
@@ -0,0 +1,24 @@
+﻿
+
+const saveButton = document.getElementById('saveBTN');
+
+
+
+saveButton.addEventListener('click', showClickedMessage);
+
+
+function showClickedMessage(e) {  // Always pass in E
+    //e.preventDefault();
+
+    var source = document.getElementById('source').value;
+    var destination = document.getElementById('destination').value;
+
+    var myData = { Source: source, Destination: destination }; 
+    $.ajax({
+        type: 'POST',
+        data: myData, 
+        url: '/Home/UpdateSourceAndDestination',
+    });
+
+   
+}
